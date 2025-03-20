7887be433580606420ca58e9feb521bac4996286
Mihai <mihaimirca918@gmail.com>
2025-03-20 07:59:30 +0200

    Am adaugat tema pentru acasa

---
+++ Tekwill/Projects/src/lesson4homework/Variables.java
@@ -0,0 +1,17 @@
+package lesson4homework;
+
+public class Variables {
+    public static void main(String[] args) {
+        long currentWorldPopulation = 7811080815L;
+        int carsProducedThisYear = 55235358;
+        char firstLetterInAlphabet = 'A';
+        boolean javaIsAmazing = true;
+        double pi = 3.14159;
+        System.out.println(currentWorldPopulation);
+        System.out.println(carsProducedThisYear);
+        System.out.println(firstLetterInAlphabet);
+        System.out.println(javaIsAmazing);
+        System.out.println(pi);
+    }
+}
+
